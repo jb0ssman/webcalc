d24b3243e88cab9aa229acf0fb8b6169f2258e36
jb0ssman <jboss@hagerty.com>
2017-06-09 13:58:51 -0400

    I have no idea if I did this correctly

--- test_webcalc.py
+++ test_webcalc.py
@@ -8,7 +8,6 @@ from webcalc import app
 def client():
     return app.test_client()
 
-
 def describe_index():
 
     def it_says_hello(client):
@@ -22,5 +21,23 @@ def describe_calc():
             response = client.get('/4/+/5')
 
             expect(response.data).contains(b"9")
+
+@pytest.fixture
+def pattern():
+    with app.app_context():
+        mongo.db.operations.drop()
+        mongo.db.operations.insert(
+            dict(
+                name="x",
+                pattern="{{ a * b }}"
+            )
+        )
+
+def describe_calc():
+ 
+        def from_db(client, pattern):
+            response = client.get('/4/x/5')
+
+            expect(response.data).contains(b"20")
 
 
